c5426cfed6109b36d50faaab3fe3f0847de3637c
Rishabh Jain <rishabh101.99@gmail.com>
2019-06-05 07:52:37 -0700

    ade very basic front end for search engine

---
+++ BiasedSearchEngine/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>The Search Engine</title>
+	</head>
+	<body>
+		<form method="get" action="results.php">
+			<label>What would you like to search for?</label>
+			<input type="text" name="search">
+			<button type="submit">Search</button>
+		</form>
+	</body>
+</html>
