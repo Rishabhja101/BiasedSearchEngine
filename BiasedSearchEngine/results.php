96dc5de0c69c216c6b62125bae1cd6becf53e6ed
Rishabh Jain <rishabh101.99@gmail.com>
2019-06-05 17:35:40 -0700

    exact name matching is working

--- BiasedSearchEngine/results.php
+++ BiasedSearchEngine/results.php
@@ -1,6 +1,15 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "") or die(mysqli_error());
-    mysqli_select_db($con, "biasedSearchEngine") or die(mysqli_error());
+    $con = mysqli_connect("localhost", "root", "") or die(mysqli_error($con));
+    mysqli_select_db($con, "biasedSearchEngine") or die(mysqli_error($con));
 
-  
+    $clean = mysqli_real_escape_string($con, $_GET['search']);
+    $hello = mysqli_query($con, "SELECT * FROM items WHERE title = '$clean'") or die(mysqli_error($con));
+    if(mysqli_num_rows($hello) >= 1){
+        while($i = mysqli_fetch_array($hello)){
+            echo '<a href="'.$i['url'].'">'.$i['title'].'</a><p>'. $i['url']. '<p>'. $i['description'].'<p><a>-------------------------------------------------------------------------------------</a><p>';
+        }
+    }
+    else {
+        echo "No results found, sorry :(";
+    }
 ?>
