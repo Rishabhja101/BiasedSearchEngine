036977056cae7b1884240b08a0fa199d1f2be65f
Rishabh Jain <rishabh101.99@gmail.com>
2019-06-12 20:17:54 -0700

    added quick sort function

--- BiasedSearchEngine/results.php
+++ BiasedSearchEngine/results.php
@@ -3,7 +3,7 @@
     mysqli_select_db($con, "biasedSearchEngine") or die(mysqli_error($con));
 
     $clean = mysqli_real_escape_string($con, $_GET['search']);
-    $hello = mysqli_query($con, "SELECT * FROM items WHERE title = '$clean'") or die(mysqli_error($con));
+    $hello = mysqli_query($con, "SELECT * FROM items WHERE title LIKE '%$clean%'") or die(mysqli_error($con));
     if(mysqli_num_rows($hello) >= 1){
         $results = array();
         $occurrences = array();
@@ -11,8 +11,9 @@
             array_push($results, '<a href="<?php redirect('.$i['url'].') ?>">'.$i['title'].'</a><p>'. $i['url']. '<p>'. $i['description'].'<p>');
             array_push($occurrences, $i['activity']);
         }
-        $results = sorter($results, $occurrences);
-        $occurrences = sorter2($results, $occurrences);
+        quickSort($occurrences, 0, sizeof($results), $results);
+//        $results = sorter($results, $occurrences);
+//        $occurrences = sorter2($results, $occurrences);
         for ($n = 0; $n < sizeof($results); $n++){
             echo $results[$n];
             echo $occurrences[$n];
@@ -28,6 +29,41 @@
         window.open('http://destination.com');
         //header($url, true, $permanent ? 301 : 302);
         exit();
+    }
+
+    function quickSort($array, $start, $end, $array0){
+        if ($start < $end){
+            $piv_pos = partition($array, $start, $end, $array0);
+            quickSort($array, $start, $piv_pos - 1, $array0);
+            quickSort($array, $piv_pos + 1, $end, $array0);
+        }
+    }
+
+    function partition($array, $start, $end, $array0){
+        $i = $start + 1;
+        $piv = $array[$start];
+        for ($j = $start + 1; $j <= $end; $j++){
+            if ($arrayp[$j] < $piv){
+                $temp = $array[$i];
+                $array[$i] = $array[$j];
+                $array[$j] = $temp;
+
+                $temp = $array0[$i];
+                $array0[$i] = $array0[$j];
+                $array0[$j] = $temp;
+
+                $i++;
+            }
+        }
+        $temp = $array[$start];
+        $array[$start] = $array[$i - 1];
+        $array[$i - 1] = $temp;
+
+        $temp = $array0[$start];
+        $array0[$start] = $array0[$i - 1];
+        $array0[$i - 1] = $temp;
+
+        return $i - 1;
     }
 
     function sorter($array1, $array2) {
