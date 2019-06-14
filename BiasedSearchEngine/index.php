2f463b45cecf301c60d29d041e431b1be82c27a2
Rishabh Jain <rishabh101.99@gmail.com>
2019-06-13 23:20:58 -0700

    final touches

--- BiasedSearchEngine/index.php
+++ BiasedSearchEngine/index.php
@@ -3,14 +3,16 @@
 	<head>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-		<title>The Search Engine</title>
+		<title>BSSEARCHER</title>
 	</head>
-	<body class="container">
+	<body class="container" style="padding-top:150px;">
 		<h1 class="text-center display-4">
 			<a style="font-size:55px; color:blue;">B</a><a style="font-size:53px; color:red;">S</a><strong style="font-size:51px; color:grey; font-weight: 800;">S</strong><strong style="font-size:49px; color:blue; font-weight: 800;">E</strong><strong style="font-size:47px; color:green; font-weight: 800;">A</strong><strong style="font-size:47px; color:red; font-weight: 800;">R</strong><strong style="font-size:49px; color:purple; font-weight: 800;">C</strong><strong style="font-size:51px; color:brown; font-weight: 800;">H</strong><a style="font-size:53px; color:orange">E</a><a style="font-size:55px; color:green">R</a>
 		</h1>
-		<form class="form-group" method="get" action="results.php" style="text-align:center">
-			<input class="form-control mb-4" type="text" name="search">
+		<form class="form-group" method="get" action="results.php" style="text-align:center;">
+			<div style="padding-left:20%; padding-right:20%;">
+				<input class="form-control mb-4" type="text" name="search">
+			</div>
 			<button class="btn btn-lg btn-primary" style="width: 20%;" type="submit">Search</button>
 		</form>
 	</body>
@@ -19,8 +21,8 @@
 	    <h3>
 			Background information about BSSEARCHER:
 		</h3>
-		<h6>
-			Currently, there are no industry standards for how corporations regulate the development and training of their AI algorithms to limit bias. This leaves the public in danger of being abused by these corporations since they will unknowingly be using these bias algorithms which they blindly trust to provide them with unbiased information. Ideally, the govenment should set industry standards for processes of developing and training AI algorithms to limit inherent bias. To demonstrate this problem with, I have developed simple search engine ("BSSEARCHER") which I have trained to be biased and right-winged in politics to demonstrate how corporations that are unregulated may be able to allow bias into thier services which manipulates their consumers.
-		</h6>
+		<p style="text-align:justify;">
+			Currently, there are no industry standards for how corporations regulate the development and training of their AI algorithms to limit bias. This leaves the public in danger of being abused by these corporations since they will unknowingly be using these bias algorithms which they blindly trust to provide them with unbiased information. Ideally, the govenment should set industry standards for processes of developing and training AI algorithms to limit inherent bias. To demonstrate this problem with, I have developed simple search engine ("BSSEARCHER") which I have trained to be biased and right-winged in politics to demonstrate how corporations that are unregulated may be able to allow bias into thier services which manipulates their consumers. Here is a link to a more in-depth explanation of the problem along with the proposed solution in an essay about why we need <a href="https://docs.google.com/document/d/1z1vpHm3PhNq2tdGRLLQwA5cFtVScbh51Xtbyk2gVIQs/edit?usp=sharing">Government Regulation on AI</a>.
+		</p>
 	</footer>
 </html>
